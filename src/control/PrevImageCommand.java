1d4b6de733d2759916ea8f6034df7338e71410ff
J0RG3PD99 <J0RG3PD99@MSI>
2020-02-04 17:05:23 +0000

    ImageViewer finished

---
+++ src/control/PrevImageCommand.java
@@ -0,0 +1,16 @@
+package control;
+
+import view.ImageDisplay;
+
+public class PrevImageCommand implements Command{
+     private ImageDisplay imageDisplay;
+
+    public PrevImageCommand(ImageDisplay imageDisplay) {
+        this.imageDisplay = imageDisplay;
+    }
+
+    @Override
+    public void execute() {
+        imageDisplay.show(imageDisplay.image().prev());
+    }
+}
